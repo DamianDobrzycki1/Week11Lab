2cfed931f98127458ec3883a32c9a05e3cc5e81e
Damian <G00419511@atu.ie>
2023-12-05 13:26:52 +0000

    Added new code in Main and added an Override for toString

--- src/main/java/atu/ie/Person.java
+++ src/main/java/atu/ie/Person.java
@@ -6,19 +6,42 @@ public class Person {
     String address;
     String telephone;
 
-    public Person(){
+    public Person(String name, String address, String telephone){
         this.name = " ";
         this.address = " ";
         this.telephone = " ";
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
-    public void setAddress(String address){
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
     }
-    public void setTelephone(String telephone){
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
         this.telephone = telephone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", telephone='" + telephone + '\'' +
+                '}';
     }
 }
